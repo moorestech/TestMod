18c2e61ceb8f68593bbb50a539150292ba8dba91
sakastudio <sakastudio100@gmail.com>
2023-04-30 18:00:02 +0900

    inital commit

---
+++ TestMod/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Mod.Base;
+
+namespace TestMod
+{
+    public class Class1 : MoorestechServerModEntryPoint 
+    {
+        public override void OnLoad(ServerModEntryInterface serverModEntryInterface)
+        {
+            Console.WriteLine("Mod Loaded!");
+        }
+    }
+}
